b422a9bfb458751c0f530f41517a146282438423
Heidy Pastor <heidypastor@gmail.com>
2022-12-07 13:59:21 -0500

    form and model language

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,7 @@
 class Language < ApplicationRecord
   has_many :users_languages
   has_many :users, through: :users_languages
+  LANGUAGES = %w[English French Spanish German Japanes Chinois Italian Arabic Hindi Portuguese Russian Korean].freeze
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, inclusion: { in: LANGUAGES }
 end
