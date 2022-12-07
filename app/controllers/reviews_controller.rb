acddb876a8fe34303d0f567c375d65a4bfef5d4e
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-12-07 13:18:36 -0500

    controllers seed models review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,29 @@
 class ReviewsController < ApplicationController
-  def index
-    @review = Review.all
-  end
+  # before_action :set_restaurant, only: %i[new create]
 
   def new
-    @user = User.find(params[:user_id])
+    @user = User.find_by_id(params[:user_id])
     @review = Review.new
+  end
+
+  def show
+    @review = Review.find_by_id(params[:id])
   end
 
   def create
     @review = Review.new(review_params)
     @review.user = @user
-    @review.save
-    redirect_to user_path(@user)
+    @review.save!
+    redirect_to review_path(@review)
   end
 
   private
 
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:commentary, :rating)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,133 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  email: "heidy@hotmail.com",
+  password: 123456,
+  username: "Heidy",
+  friend: false,
+  origin: "Colombiene",
+  province: "Cartagena",
+  address: "Chateau De Saint Felipe",
+  latitude: 1.1,
+  longitude: 111.0
+)
+
+User.create!(
+  email: "pao@hotmail.com",
+  password: 123456,
+  username: "Pao",
+  friend: false,
+  origin: "Colombiene",
+  province: "Armenia",
+  address: "Ville Caroline",
+  latitude: 2.1,
+  longitude: 222.0
+)
+User.create!(
+  email: "felipe@hotmail.com",
+  password: 123456,
+  username: "Felipe",
+  friend: false,
+  origin: "Colombien",
+  province: "Santa Marta",
+  address: "El Rodadero",
+  latitude: 3.1,
+  longitude: 333.0
+)
+
+User.create!(
+  email: "andres@hotmail.com",
+  password: 123456,
+  username: "Andres",
+  friend: false,
+  origin: "Colombien",
+  province: "Medellin",
+  address: "El poblado",
+  latitude: 4.1,
+  longitude: 444.0
+)
+
+User.create!(
+  email: "sebastian@hotmail.com",
+  password: 123456,
+  username: "Sebastian",
+  friend: false,
+  origin: "Colombien",
+  province: "Bogota",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
+
+User.create!(
+  email: "sebastian@hotmail.com",
+  password: 123456,
+  username: "Sebastian",
+  friend: false,
+  origin: "Colombien",
+  province: "Bogota",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
+
+User.create!(
+  email: "estuardo@hotmail.com",
+  password: 123456,
+  username: "Estuardo",
+  friend: true,
+  origin: "Candien",
+  province: "Quebec",
+  address: "Montegeri",
+  latitude: 6.1,
+  longitude: 666.0
+)
+
+User.create!(
+  email: "etienne@hotmail.com",
+  password: 123456,
+  username: "Etienne",
+  friend: true,
+  origin: "canadien",
+  province: "Toronto",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
+
+User.create!(
+  email: "sebastian@hotmail.com",
+  password: 123456,
+  username: "Sebastian",
+  friend: true,
+  origin: "Colombien",
+  province: "Bogota",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
+
+User.create!(
+  email: "sebastian@hotmail.com",
+  password: 123456,
+  username: "Sebastian",
+  friend: true,
+  origin: "Colombien",
+  province: "Bogota",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
+
+User.create!(
+  email: "sebastian@hotmail.com",
+  password: 123456,
+  username: "Sebastian",
+  friend: true,
+  origin: "Colombien",
+  province: "Bogota",
+  address: "Centro",
+  latitude: 5.1,
+  longitude: 555.0
+)
