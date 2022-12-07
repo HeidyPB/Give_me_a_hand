aaea2c7689c42c7e9c66a0588f777f6f736d15e4
paularosero <pao-0114@hotmail.com>
2022-12-07 12:19:49 -0500

    friends funtion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  email:"pao@hotmail.com",
+  password:123456,
+  username:"pao",
+  friend: false,
+  origin: "Colombien",
+  province: "armenia",
+  address: "villa",
+  latitude: 1.1,
+  longitude: 222.0
+
+)
+User.create!(
+  email:"friend@hotmail.com",
+  password:123456,
+  username:"pao",
+  friend: true,
+  origin: "Colombien",
+  province: "armenia",
+  address: "villa",
+  latitude: 1.1,
+  longitude: 222.0
+)
