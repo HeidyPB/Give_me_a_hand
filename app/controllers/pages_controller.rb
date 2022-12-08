7327dfee41144a2cb6cb2d2fa27642bf14ad2dde
Heidy Pastor <heidypastor@gmail.com>
2022-12-08 11:50:26 -0500

    merge conflit resolve

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,56 +4,7 @@ class PagesController < ApplicationController
   def home
   end
 
-<<<<<<< HEAD
-  def friends
-    @friends = User.where(friend: true)
-  end
-
-  def new
-    @friend = User.new
-    # authorize @friend
-  end
-
-  def create
-    @friend = User.new(friend_params)
-    @friend.user = current_user
-    # authorize @friend
-    if @friend.save
-      redirect_to friend_path(@friend), notice: 'friend was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @friend.update(friend_params)
-      redirect_to friend_path(@friend)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @friend.destroy
-    redirect_to friends_path(@friend)
-  end
-
-  private
-
-  def set_friend
-    @friend = User.find(params[:id])
-    authorize @friend
-  end
-
-  def friend_params
-    params.require(:friend).permit(:username, :origin, :province, :address, :language, :category, :photo)
-  end
-=======
   # def friends
   #   @friends = User.where(friend: true)
   # end
->>>>>>> b036d1f99a5c0d2c3a42bc7ee88b18965f48df1e
 end
