8ed639617f6608e79d73f83eec48da3f58ab9000
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-12-08 16:29:06 -0500

    generated create profile page inside controllers, its route, and the def

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   # friends routes
   get 'friends', to: 'friends#index'
   get "friends/new", to: "friends#new"
-  post "friends", to: "friends#create", as: :new_friend
+  patch "friends", to: "friends#update_user", as: :update_user
+  # get "friends/edit", to: "friends#edit", as: :edit_friend
+  # patch "friends/:id", to: "friends#update", as: :update_friend
 
+  # meetings routes
   get 'users/:id/meeting', to: 'meetings#show', as: :meeting
-  
+
+  # nested routes
   resources :friends, only: [:show] do
     resources :meetings, only: %i[new create]
   end
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
