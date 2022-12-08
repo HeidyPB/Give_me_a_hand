11bad11281275d01a809d9b9e45a0d4e94284daf
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-12-08 10:42:59 -0500

    removed methods from pages controller & from it's views and then implimented index & show for friends while adding its routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'friends', to: 'pages#friends'
-
+  get 'friends', to: 'friends#index'
+  get "friends/:id", to: "friends#show"
 
   resources :friends, only: [:show] do
     resources :meetings, only: %i[new create]
